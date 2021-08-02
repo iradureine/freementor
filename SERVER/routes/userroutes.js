39079f1f5646a5e2eec4b47ded68359d520d7ee1
iradureine <miradukunda601@gmail.com>
2021-08-02 10:39:20 -0700

    validation

--- SERVER/routes/userroutes.js
+++ SERVER/routes/userroutes.js
@@ -1,10 +1,15 @@
 import express from "express";
+import Validator from "../middleware/validator.js"
 import UserController from "../controllers/usercontroller.js";
 
 
  const userRouter= express.Router();
 
-userRouter.post("/signup",UserController.signupUser);
+userRouter.post("/signup",
+Validator.newAccountRules(),
+Validator.validateInput,
+UserController.signupUser);
+
 userRouter.get("/all",UserController.getAllUsers);
 userRouter.get("/:id",UserController.getoneUser);
 userRouter.delete("/:id",UserController.deleteUser);
